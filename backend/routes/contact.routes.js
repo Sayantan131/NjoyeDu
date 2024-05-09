1dbc0cac095e363d4352ea2f36b288d97f43efe8
Sayantan <maitysayantan111@gmail.com>
2024-05-09 19:47:49 +0530

    Fronted Complete

--- backend/routes/contact.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import sendEmail from "../controllers/sendEmail.controller.js";
-
-const app = express.Router();
-
-app.post("/send-email", sendEmail);
-
-export default app;
